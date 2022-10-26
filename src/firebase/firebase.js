a2c15f2d22b6d40c714d6bb0d2dfc5a90c032799
Maryana182 <111122257+Maryana182@users.noreply.github.com>
2022-10-26 12:15:29 +0300

    Setup test database env

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,12 +1,12 @@
 import * as firebase from 'firebase';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDHrA9cuhy58bZx3du76nM6y-hdLmE8S1k",
-    authDomain: "expensify-84a75.firebaseapp.com",
-    databaseURL: "https://expensify-84a75-default-rtdb.firebaseio.com",
-    projectId: "expensify-84a75",
-    storageBucket: "expensify-84a75.appspot.com",
-    messagingSenderId: "1034820492366",
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
     appId: "1:1034820492366:web:f06395930a0755dd8bb71f",
     measurementId: "G-WTKSN06NF4"
   };
